24d11b5f3e6d9ec59d2cedc5acce5f7a049902ff
Rachid Groeneveld <Saiyato@users.noreply.github.com>
2022-01-21 22:11:52 +0100

    Prepare for Volumio 3
    Added the newest versions from the maintainer, packaged them in the plugin and updated the package.json for Volumio 3

--- install.sh
+++ install.sh
@@ -8,17 +8,33 @@ if [ ! -f $INSTALLING ]; then
 
 	if [ ! -d /opt/squeezelite ];
 	then 
-		# Download latest squeezelite executable
-		echo "Downloading squeezelite..."
-		wget -O /opt/squeezelite https://github.com/Saiyato/volumio-squeezelite-plugin/raw/master/known_working_versions/squeezelite-armv6hf-volumio
+		# Download squeezelite executable
+		dist=$(cat /etc/os-release | grep '^VERSION=' | cut -d '(' -f2 | tr -d ')"')
+		arch=$(arch)
+		
+		if [ $dist = "jessie" ] && ( [ $arch = "armv6l" ] || [ $arch = "armv7l" ] || [ $arch = "aarch64" ] ); then
+			echo "Using squeezelite 1.8.7 for compatibility reasons (detected Debian Jessie)"
+			ln -fs /data/plugins/music_service/squeezelite/known_working_versions/jessie/squeezelite-armv6hf-volumio /opt/squeezelite
+		elif [ $dist = "buster" ] && ( [ $arch = "armv6l" ] || [ $arch = "armv7l" ] ); then
+			echo "Using squeezelite 1.9.9 for armhf architecture"
+			ln -fs /data/plugins/music_service/squeezelite/known_working_versions/squeezelite-1.9.9.1392-armhf /opt/squeezelite
+		elif [ $dist = "buster" ] && ( [ $arch = "aarch64" ] ); then
+			echo "Using squeezelite 1.9.9 for aarch64 architecture"
+			ln -fs /data/plugins/music_service/squeezelite/known_working_versions/squeezelite-1.9.9.1392-aarch64 /opt/squeezelite
+		elif [ $dist = "buster" ] && ( [ $arch = "x86_64" ] ); then
+			echo "Using squeezelite 1.9.9 for x86_64 architecture"
+			ln -fs /data/plugins/music_service/squeezelite/known_working_versions/squeezelite-1.9.9.1392-x86_64 /opt/squeezelite
+		elif [ $dist = "buster" ] && ( [ $arch = "i686" ] ); then
+			echo "Using squeezelite 1.9.9 for i686 architecture"
+			ln -fs /data/plugins/music_service/squeezelite/known_working_versions/squeezelite-1.9.9.1392-i686 /opt/squeezelite
+		fi
 				
 		# Fix executable rights
 		chown volumio:volumio /opt/squeezelite
 		chmod 755 /opt/squeezelite
 		
 		# Download and activate default unit
-		TMPUNIT="/data/plugins/music_service/squeezelite/squeezelite.service"
-		wget -O $TMPUNIT https://raw.githubusercontent.com/Saiyato/volumio-squeezelite-plugin/master/unit/squeezelite.unit-template
+		TMPUNIT="/data/plugins/music_service/squeezelite/unit/squeezelite.service"
 		chown volumio $TMPUNIT
 		
 		sed 's|${NAME}|-n Volumio|g' -i $TMPUNIT
@@ -28,7 +44,7 @@ if [ ! -f $INSTALLING ]; then
 		sed 's|${EXTRA_PARAMS}||g' -i $TMPUNIT
 		
 		#mv $TMPUNIT /etc/systemd/system/squeezelite.service
-		ln -fs /data/plugins/music_service/squeezelite/squeezelite.service /etc/systemd/system/squeezelite.service		
+		ln -fs /data/plugins/music_service/squeezelite/squeezelite.service /etc/systemd/system/squeezelite.service
 		systemctl daemon-reload
 		
 	else
