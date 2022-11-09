35b4cbd229df3eb186be82d06744d8a5a8956ccc
marcus <95848098+itskaefer@users.noreply.github.com>
2022-11-09 15:12:32 +0100

    minor changes

--- README.md
+++ README.md
@@ -5,26 +5,27 @@ Not much to say about it, it's pretty straight forward.
 
 It starts with default values:
 
--o default
--n Volumio
--a 80:4::
--s 127.0.0.1
--r 44100-196000 -R vE:::24
+-o default -n Volumio -a 80:4::
 
 
 # changed by itskaefer
 Added updates so it'll work on a miniDSP SHD with the NEO3 board.
 It should also enable you to use the remote control for next,previous,pause,play
 
-Unfortunatly a straight forward install on a minidsp isn't possible because of the ssh-user
-from minidsp.
+-o default
+-n minidsp-shd
+-a 80:4::
+-s 127.0.0.1
+-r 44100-192000 -R vE:::24
+
 
 INSTALL on miniDSP SHD
-1. install the available squeezelite-plugin on volumio
+1. install the available squeezelite-plugin on volumio (don't activate it)
 
 2. log into miniDSP SHD by SSH and execute the following command:
 
 wget -O - https://github.com/itskaefer/volumio-squeezelite-plugin/raw/master/update_squeezelite_minidsp.sh | sudo sh
 
-3. go to the "installed plugins" in volumio and save the configuration of the squeezelite plugin to enable changes
+3. go to the "installed plugins" in volumio and activate the plugin.
+   click "Settings" and change the IP to your LMS, save the configuration and squeezelite should start
 
--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [ ! -f $INSTALLING ]; then
     variant=$(cat /etc/os-release | grep '^VOLUMIO_VARIANT=' | cut -d '=' -f2 | tr -d '"')
 
     if [ $dist = "jessie" ] && [ $variant = "minidspshd" ] && ( [ $arch = "armv6l" ] || [ $arch = "armv7l" ] || [ $arch = "aarch64" ] ); then
-      echo "Using squeezelite 1.9.9 armel architecture (detected minidsp SHD)"
+      echo "Using squeezelite 1.9.9 armel static architecture (detected minidsp SHD)"
       ln -fs /data/plugins/music_service/squeezelite/known_working_versions/jessie/squeezelite-arm-minidspshd /opt/squeezelite
     elif [ $dist = "jessie" ] && ( [ $arch = "armv6l" ] || [ $arch = "armv7l" ] || [ $arch = "aarch64" ] ); then
       echo "Using squeezelite 1.8.7 for compatibility reasons (detected Debian Jessie)"
@@ -29,20 +29,39 @@ if [ ! -f $INSTALLING ]; then
 			echo "Using squeezelite 1.9.9 for i686 architecture"
 			ln -fs /data/plugins/music_service/squeezelite/known_working_versions/squeezelite-1.9.9.1392-i686 /opt/squeezelite
 		fi
-				
-		# Fix executable rights
+
+    # Fix executable rights
 		chown volumio:volumio /opt/squeezelite
 		chmod 755 /opt/squeezelite
 		
-		# Download and activate default unit
+		# activate default unit
 		TMPUNIT="/data/plugins/music_service/squeezelite/unit/squeezelite.service"
 		chown volumio $TMPUNIT
-		
+
 		sed 's|${NAME}|-n Volumio|g' -i $TMPUNIT
 		sed 's|${OUTPUT_DEVICE}|-o default|g' -i $TMPUNIT
 		sed 's|${ALSA_PARAMS}|-a 80:4::|g' -i $TMPUNIT
     sed 's|${SERVER_PARAMS}|-s 127.0.0.1|g' -i $TMPUNIT
-		sed 's|${EXTRA_PARAMS}||g' -i $TMPUNIT
+    # sed 's|${EXTRA_PARAMS}||g' -i $TMPUNIT  - moved down
+
+    if [ $variant = "minidspshd" ]; then
+      pluginPath="/data/plugins/music_service"
+      pluginInputs="/volumio/app/plugins/music_service/inputs/index.js"
+      echo "Add special config for minidsp ..."
+      cp $pluginPath/squeezelite/config_minidsp.json $pluginPath/squeezelite/config.json
+      if [ $(grep -c "squeezelite" $pluginInputs ) -eq 0 ]; then
+        sed -i "s/self.commandRouter.volumioToggle();/self.commandRouter.volumioToggle();\nthis.commandRouter.executeOnPlugin('music_service', 'squeezelite', 'pause');/g" $pluginInputs
+        sed -i "s/self.commandRouter.volumioPrevious();/self.commandRouter.volumioPrevious();\nthis.commandRouter.executeOnPlugin('music_service', 'squeezelite', 'previousSong');/g" $pluginInputs
+        sed -i "s/self.commandRouter.volumioNext();/self.commandRouter.volumioNext();\nthis.commandRouter.executeOnPlugin('music_service', 'squeezelite', 'nextSong');/g" $pluginInputs
+      else
+        echo "Plugin inputs already updated ..."
+      fi
+      echo "Fix permissions ..."
+      chown -R volumio:volumio $pluginPath/squeezelite
+      sed 's|${EXTRA_PARAMS}|-r 44100-196000 -R vE:::24|g' -i $TMPUNIT
+    else
+      sed 's|${EXTRA_PARAMS}||g' -i $TMPUNIT
+    fi
 		
 		#mv $TMPUNIT /etc/systemd/system/squeezelite.service
 		ln -fs /data/plugins/music_service/squeezelite/unit/squeezelite.service /etc/systemd/system/squeezelite.service
